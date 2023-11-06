93ecd70d9c2ddb7d4486d7fea1feec5ecc492e17
Nazarii <nazariikubik@gmail.com>
2023-11-06 12:52:33 -0800

    check

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -22,7 +22,8 @@ export default async function Navbar() {
           <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
             <LogoSquare />
             <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {SITE_NAME}
+              {/* {SITE_NAME} */}
+              HALLO
             </div>
           </Link>
           {menu.length ? (
