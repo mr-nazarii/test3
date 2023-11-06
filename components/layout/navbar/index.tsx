adcfefe685ce99a3b9999d0b35954b02e5dd12c4
Nazarii <nazariikubik@gmail.com>
2023-11-06 13:58:28 -0800

    new update

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -7,32 +7,34 @@ import Link from 'next/link';
 import { Suspense } from 'react';
 import MobileMenu from './mobile-menu';
 import Search from './search';
-const { SITE_NAME } = process.env;
+// const { SITE_NAME } = process.env;
 
 export default async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
 
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
+    <nav className="relative mb-5 flex items-center justify-between bg-white px-20 py-4">
       <div className="block flex-none md:hidden">
         <MobileMenu menu={menu} />
       </div>
-      <div className="flex w-full items-center">
-        <div className="flex w-full md:w-1/3">
+      <div className="flex w-full justify-between">
+        <div className="visible flex w-full  md:w-1/3">
           <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
-            <LogoSquare />
-            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {/* {SITE_NAME} */}
-              HALLO
+            {/* <LogoSquare /> */}
+            <div className="text-md ml-2  flex-none font-medium uppercase text-black md:hidden lg:block">
+              AC INFINITY
             </div>
           </Link>
+        </div>
+        <div className="hidden justify-center md:flex md:w-1/3">
+          {/* <Search /> */}
           {menu.length ? (
-            <ul className="hidden gap-6 text-sm md:flex md:items-center">
+            <ul className="hidden text-sm md:flex md:items-center">
               {menu.map((item: Menu) => (
                 <li key={item.title}>
                   <Link
                     href={item.path}
-                    className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                    className="rounded-sm px-5 py-2 font-medium text-black transition duration-300 ease-in-out hover:bg-gray-200"
                   >
                     {item.title}
                   </Link>
@@ -40,9 +42,6 @@ export default async function Navbar() {
               ))}
             </ul>
           ) : null}
-        </div>
-        <div className="hidden justify-center md:flex md:w-1/3">
-          <Search />
         </div>
         <div className="flex justify-end md:w-1/3">
           <Suspense fallback={<OpenCart />}>
